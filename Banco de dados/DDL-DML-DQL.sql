2023eff1136c89795958bd4d5fe33164783cb2c6
fic <fic@DESKTOP-0L5VRFL>
2025-08-27 20:05:11 -0300

    DDL - DML - DQL

--- Banco de dados/DDL-DML-DQL.sql
+++ Banco de dados/DDL-DML-DQL.sql
@@ -1,3 +1,10 @@
+--DDL Comandos que Criam, Editam ou Removem Tabelas, Colunas, Schema, Banco de Dados
+/* 
+
+CREATE <O QUE CRIAR>
+DROP <O QUE REMOVER>
+ALTER <O QUE EDITAR>
+*/
 CREATE SCHEMA clinica;
 --Criar TAbelas
 CREATE TABLE clinica.medico(
@@ -33,9 +40,18 @@ CREATE TABLE clinica.consulta(
 );
 
 --Excluir Tabelas
-/*
+
 DROP TABLE clinica.consulta;
 DROP TABLE clinica.clinica;
 DROP TABLE clinica.paciente;
 DROP TABLE clinica.medico;
-*/
+
+--Limpar Tabela
+TRUNCATE TABLE clinica.consulta
+--RESTART IDENTITY -Reinicia a sequencia da tabela
+
+--Editar Tabelas
+
+ALTER TABLE clinica.paciente ADD COLUMN telefone NUMERIC (11); -- adiciona coluna telefone
+ALTER TABLE clinica.paciente ADD COLUMN email TEXT UNIQUE; -- adiciona columa email em que o dado é unico na tabela
+ALTER TABLE clinica.paciente 
