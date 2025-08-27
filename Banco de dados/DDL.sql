323a65f46e61eb97c9d7ae5b9cd018f538f57b66
fic <fic@DESKTOP-0L5VRFL>
2025-08-26 21:33:36 -0300

    Adicionado schema do banco de dados

---
+++ Banco de dados/DDL.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA clinica;
+
+CREATE TABLE clinica.medico(
+	id_medico INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	nome TEXT NOT NULL,
+	crm TEXT NOT NULL,
+	especialidade TEXT NOT NULL
+);
+
+CREATE TABLE clinica.paciente(
+	id_paciente INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	cpf TEXT NOT NULL,
+	nome TEXT NOT NULL,
+	idade INT NOT NULL,
+	data_de_nascimento DATE NOT NULL
+);
+
+CREATE TABLE clinica.clinica(
+	id_clinica INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	nome INT NOT NULL,
+	endereco TEXT NOT NULL,
+	descricao TEXT NOT NULL
+);
+
+CREATE TABLE clinica.consulta(
+	id_consulta INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	data_consulta TIMESTAMPTZ NOT NULL,
+	valor NUMERIC (10,4),
+	id_medico INT NOT NULL,
+	FOREIGN KEY (id_medico) REFERENCES clinica.medico(id_medico), --metodo longo
+	id_clinica INT NOT NULL REFERENCES clinica.clinica(id_clinica), --metodo curto
+	id_paciente INT NOT NULL REFERENCES clinica.paciente(id_paciente)
+);
+	 
