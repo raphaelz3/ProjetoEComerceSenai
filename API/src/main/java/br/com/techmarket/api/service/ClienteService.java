9facc111c4455081d0aebc2a618084deaa41cd5f
fic <fic@DESKTOP-0L5VRFL>
2025-09-03 21:50:04 -0300

    Introdução ao BD

---
+++ API/src/main/java/br/com/techmarket/api/service/ClienteService.java
@@ -0,0 +1,23 @@
+package br.com.techmarket.api.service;
+
+import br.com.techmarket.api.model.Cliente;
+import br.com.techmarket.api.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+    //injeção de dependencia
+    private final ClienteRepository clienteRepository; //o "final" define a variável como const
+
+    public ClienteService(ClienteRepository repo) {
+        this.clienteRepository = repo;
+    }
+
+    //Listar clientes
+    public List<Cliente> listaTodos(){
+        return clienteRepository.findAll();
+    }
+
+}
