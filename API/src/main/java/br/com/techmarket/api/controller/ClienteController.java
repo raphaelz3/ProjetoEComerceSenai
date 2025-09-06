e2dc6a23c2ecb11e1c45b6931eb2b080f04b3fb4
fic <fic@DESKTOP-0L5VRFL>
2025-09-05 21:49:42 -0300

    Introdução a API Rest

--- API/src/main/java/br/com/techmarket/api/controller/ClienteController.java
+++ API/src/main/java/br/com/techmarket/api/controller/ClienteController.java
@@ -5,6 +5,7 @@ import br.com.techmarket.api.repository.ClienteRepository;
 import br.com.techmarket.api.service.ClienteService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import java.util.List;
 
 //indicamos qual tipo de API
 @RestController
-//aqui indicamos o DNS da API
+//aqui indicamos o URL da API - Endpoint
 @RequestMapping("/api/clientes")
 public class ClienteController {
     //Controler depende do service, então fazemos a injeção de dependência conforme abaixo
@@ -24,8 +25,8 @@ public class ClienteController {
     }
 
     //Listar todos clientes
-    @GetMapping //Essa anotação pega as informações do BD
-    public ResponseEntity<List<Cliente>> listarClientes() {
+    @GetMapping //Essa anotação pega as informações do BD e entrega ao Front
+    public ResponseEntity<List<Cliente>> listarClientes() {//ResponseEntity é quem retorna o código de resposta
         List<Cliente> clientes = clienteService.listarTodos();//Aqui pegamos a função ListarTodos de ClienteService
         return ResponseEntity.ok(clientes);
     }
